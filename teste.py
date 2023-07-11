1809615e34a75d8aecbdb12a191ffe9796a0e7c3
Gustavo Broechl <gustavobroechl25@gmail.com>
2023-07-11 19:17:01 -0300

    some changes

--- teste.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from gym import spaces
-
-teste = spaces.Box(low=np.array([-10.0, -10.0, -10.0, -10.0, -10.0, -10.0, -10.0, -10.0, -10.0]),
-                   high=np.array([80.0, 80.0, 80.0, 80.0, 80.0, 80.0, 80.0, 80.0, 80.0]),
-                   dtype=np.float32)
-for _ in range(100):
-    n = teste.sample()
-    print(n)
